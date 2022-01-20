e14d5bd7ec9d4ae4635fed2c2f63fd6df50ee4ab
zzh <1148873699@qq.com>
2022-01-20 17:14:33 +0800

    first commit

---
+++ src/io/zzh/demo/design/tttt.java
@@ -0,0 +1,12 @@
+package io.zzh.demo.design;
+
+/**
+ * @author ：zzh
+ * @description ：
+ * @date ：Created in 2022/1/20 17:14
+ */
+public class tttt {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
